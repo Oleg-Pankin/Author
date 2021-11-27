483ff11fe38d16f4661dd27bafcc09a5ff5b7af1
Олег <alexpankin1972@gmail.com>
2021-11-27 11:50:12 +0500

    Author

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
-with open(
-    'oleg.txt','r', encoding = 'utf-8'
-    )as file:
-    data = file.read(1)
-    print(data)
-    data = file.read()
-    print(data)
-author = input('Автор:')
-quote = ''
-while quote!='нет':
-    quote = input('Я жду твою цитвту олвег:')
-    b = ('\n')
-    author1 = input('Я все еще жду автрроа олвег:')
-    a = (quote+b+author1)
-
-
-with open(
-    'oleg.txt','a',encoding = 'utf-8'
-    )as file:
-    data = file.write('\n')
-    data = file.write(author)
-    data = file.write('\n')
-    data = file.write('\n')
-    data = file.write(a)
-with open(
-    'oleg.txt','r', encoding = 'utf-8'
-    )as file:
-    data = file.read(1)
-    print(data)
-    data = file.read()
-    print(data)
+while True:
+    try:
+        name = input('Название файла:')
+        with open(
+            name,'r', encoding = 'utf-8'
+            )as file:
+            data = file.read(1)
+            print(data)
+            data = file.read()
+            print(data)
+        author = input('Автор:')
+        quote = ''
+        a = ''
+        with open(
+            name,'a',encoding = 'utf-8'
+            )as file:
+            file.write('\n')
+            file.write('({})'.format(author))
+            file.write('\n')
+            while quote!='нет':
+                file.write(a)
+                quote = input('Я жду твою цитвту олвег:')
+                b = '\n'
+                author1 = input('Я все еще жду автрроа олвег:')
+                a = quote+b+'({})'.format(author1)
+                file.write('\n')
+        with open(
+            name,'r', encoding = 'utf-8'
+            )as file:
+            data = file.read(1)
+            print(data)
+            data = file.read()
+            print(data)
+            break
+    except:
+        print('Файла несувщеcтвует')
